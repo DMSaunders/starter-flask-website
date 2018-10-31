6b0791b7cb7b36b18a7cb95fd7fad4376c45b91f
DMSaunders <dmariesaunders@gmail.com>
2018-10-31 10:23:41 -0700

    mpg button

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)   #init
 def home():
     return render_template('home.html') #'<p> Hello World </p>'  #content
 
+@app.route('/mpg', methods = ['GET'])  # this is the URL on the webbrowser
+def mpg():
+    return render_template('mpg.html')  #content
+
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 3333, debug = True)
